40e486a2176938c2f91fbf638b4f718273002bf1
Łukasz Sopoćko <lukasz.sopocko@gmail.com>
2022-01-20 20:58:43 +0100

    feat: infobox

---
+++ src/ui/Infobox.tsx
@@ -0,0 +1,64 @@
+import { useCallback, useEffect, useState } from "react";
+import styled from "styled-components";
+
+interface InfoboxProps {
+    className?: string;
+    title: string;
+    description: string;
+    date: string;
+}
+
+function Infobox({ className, title, description, date }: InfoboxProps) {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleDescription = useCallback(() => {
+        setIsExpanded(!isExpanded);
+    }, [isExpanded]);
+
+    let trimmedTitle = title.length > 36 ? `${title.substring(0, 36).trim()}...` : title;
+
+    return (
+        <div className={className}>
+            <h1 title={title} onClick={toggleDescription}><small>{ date }</small> { trimmedTitle } { isExpanded ? <>&laquo;</> : <>&raquo;</>}</h1>
+            <p className={`description ${isExpanded && 'is-expanded'}`}>{ description }</p>
+        </div>
+    )
+}
+
+export default styled(Infobox)`
+    color: #fff;
+
+    h1 {
+        padding: 10px;
+        background: rgba(0, 0, 0, 0.8);
+        border-top-right-radius: 5px;
+        border-bottom-right-radius: 5px;
+        font-size: 16px;
+        font-weight: 500;
+        margin: 0;
+        width: 80%;
+        height: 30px;
+        position: relative;
+        cursor: pointer;
+        
+        small {
+            display: block;
+            font-size: 10px;
+            line-height: 11px;
+            opacity: 0.6;
+        }
+    }
+
+    .description {
+        font-size: 12px;
+        line-height: 16px;
+        display: none;
+        padding: 10px;
+        margin: 0;
+        background: rgba(0, 0, 0, 0.8);
+
+        &.is-expanded {
+            display: block;
+        }
+    }
+`;
