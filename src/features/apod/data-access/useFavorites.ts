f659b078ec1eb4750c0e658a1c8fafc3ba8d91d3
Łukasz Sopoćko <lukasz.sopocko@gmail.com>
2022-01-21 13:58:47 +0100

    chore: typing

--- src/features/apod/data-access/useFavorites.ts
+++ src/features/apod/data-access/useFavorites.ts
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
+import { ApodModel } from "../apod.model";
 
 const storageKey = "apod";
 
 export const useFavorites = () => {
-  const [favorites, setFavorites] = useState<any[]>([]);
+  const [favorites, setFavorites] = useState<ApodModel[]>([]);
 
   const getFavorites = (): void => {
     const storedFavorites = localStorage.getItem(storageKey);
@@ -13,7 +14,7 @@ export const useFavorites = () => {
     }
   };
 
-  const add = (apod: any): void => {
+  const add = (apod: ApodModel): void => {
     const updatedFavorites = [...favorites, apod];
     setFavorites(updatedFavorites);
     localStorage.setItem(storageKey, JSON.stringify(updatedFavorites));
--- src/util/__test__/loadImageAsync.test.ts
+++ src/util/__test__/loadImageAsync.test.ts
@@ -1,8 +1,6 @@
 import loadImage from "../loadImageAsync";
 import ImageMock from "./image.mock";
 
-global.Image = ImageMock as any;
-
 test("returns true when image is loaded", async () => {
   const isImageLoaded = await loadImage("test.jpg");
 
